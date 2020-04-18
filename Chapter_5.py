0097ca7a0f3fadd84a0dee54dd90776dbd2f04a5
Greg Boes <gboes@pistolbarrel.net>
2020-04-18 11:42:36 -0500

    Fixed 'em.'

--- Chapter_5.py
+++ Chapter_5.py
@@ -1,46 +1,60 @@
 def exercise_1():
-
+    pass
 
 def exercise_2():
+    pass
 
 
 def exercise_3():
+    pass
 
 
 def exercise_4():
+    pass
 
 
 def exercise_5():
+    pass
 
 
 def exercise_6():
+    pass
 
 
 def exercise_7():
+    pass
 
 
 def exercise_8():
+    pass
 
 
 def exercise_9():
+    pass
 
 
 def exercise_10():
+    pass
 
 
 def exercise_11():
+    pass
 
 
 def exercise_12():
+    pass
 
 
 def exercise_13():
+    pass
 
 
 def exercise_14():
+    pass
 
 
 def exercise_15():
+    pass
 
 
 exercise_1()
